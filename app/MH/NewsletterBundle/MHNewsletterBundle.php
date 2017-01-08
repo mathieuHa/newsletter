b64158d8aa8d75b83a666a1e7dd26d7fd449b10d
mathieuHa <mathieu.hanotaux@gmail.com>
2017-01-08 11:12:41 +0100

    add file missing

---
+++ app/MH/NewsletterBundle/MHNewsletterBundle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kafim
+ * Date: 08/01/2017
+ * Time: 10:54
+ */
+
+namespace MH\NewsletterBundle;
+
+
+class MHNewsletterBundle
+{
+
+    /**
+     * MHNewsletterBundle constructor.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ src/MH/NewsletterBundle/MHNewsletterBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MH\NewsletterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MHNewsletterBundle extends Bundle
+{
+}
